064235c717dddf4ed40d843c3eaa4f477b95c856
Yoariht Macedo <yoariht.mc37@gmail.com>
2021-12-17 10:03:26 -0600

    Depuracion del codigo

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -1,11 +1,15 @@
-import { getConnection, sql } from '../database/connection'
+import { getConnection, sql, queries } from '../database'
 
 export const getUsers = async (req, res) => {
-    const pool = await getConnection();
-    const result = await pool.request().query('SELECT * FROM usuario');
-    console.log(result);
+    try {
+        const pool = await getConnection();
+        const result = await pool.request().query(queries.getAllUsers);
 
-    res.json(result.recordset);
+        res.json(result.recordset);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
 };
 
 export const createNewUser = async (req, res) => {
@@ -25,33 +29,38 @@ export const createNewUser = async (req, res) => {
         img_perfil
     } = req.body;
 
-    if(email == null || email == undefined){
+    if (email == null || email == undefined) {
         email = '';
     }
-    
-    if(telefono == null || telefono == undefined){
+
+    if (telefono == null || telefono == undefined) {
         telefono = '';
     }
-    
-    if(img_perfil == null || img_perfil == undefined){
+
+    if (img_perfil == null || img_perfil == undefined) {
         img_perfil = null;
     }
 
-    console.log(num_empleado,
-        nombre,
-        ap_paterno,
-        ap_materno,
-        departamento,
-        tipo_usuario,
-        email,
-        telefono,
-        img_perfil);
+    try {
+        console.log(num_empleado,
+            nombre,
+            ap_paterno,
+            ap_materno,
+            departamento,
+            tipo_usuario,
+            email,
+            telefono,
+            img_perfil);
 
-    //const pool = getConnection()
-    
-    //await pool.request().input("nombre", sql.VarChar, nombre).query("INSERT INTO usuario (nombre) VALUES (@nombre)")
-    
-    let hi = 'New User. Welcome ' + nombre
+        //const pool = getConnection()
 
-    res.json(hi);
+        //await pool.request().input("nombre", sql.VarChar, nombre).query("INSERT INTO usuario (nombre) VALUES (@nombre)")
+
+        let hi = 'New User. Welcome ' + nombre
+
+        res.json(hi);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
 }
